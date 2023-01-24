512b8e138e5c5d9ba0c6edb8ea7edafc878becaa
czaper0 <kacpermurat@gmail.com>
2023-01-24 22:56:31 +0100

    trivium.py

---
+++ trivium.py
@@ -0,0 +1,78 @@
+import os
+from Crypto.Util.strxor import strxor
+from Crypto.Util.number import long_to_bytes, bytes_to_long
+import random
+class Trivium:
+    def __init__(self):
+        self.state = [0]*288
+
+    def setup(self):
+        key  = self.key
+        iv  = self.iv
+        t = [0]*3
+        self.state[:92] = [*key, *[0]*12]
+        self.state[93:176] = [*iv, *[0]*3]
+        self.state[177:287] = [*[0]*107, 1, 1, 1]
+        
+        for _ in range(4*288):
+            t[0] = self.state[65] ^ self.state[90] & self.state[91] ^ self.state[92] ^ self.state[170]
+            t[1] = self.state[161] ^ self.state[174] & self.state[175] ^ self.state[176] ^ self.state[263]
+            t[2] = self.state[242] ^ self.state[285] & self.state[286] ^ self.state[287] ^ self.state[68]
+
+            self.state[0:92] = [t[2], *self.state[0:91]]
+            self.state[93:176] = [t[0], *self.state[93:175]]
+            self.state[177:287] = [t[1], *self.state[177:286]]
+    
+    def generate_key(self):
+        self.key = [random.randint(0,1) for _ in range(80)]
+        self.iv = [random.randint(0,1) for _ in range(80)]
+
+    def algorithm(self,plaintext):
+        N = len(plaintext[2:])*4
+        z, t = [], [0]*3
+        # print()
+        # print(len(self.state),len(self.key),len(self.iv))
+        self.setup()
+
+        for _ in range(N):
+            t[0] = self.state[65] ^ self.state[92]
+            t[1] = self.state[161] ^ self.state[176]
+            t[2] = self.state[242] ^ self.state[287]
+            
+            z.append(t[0] ^ t[1] ^ t[2])
+            
+            t[0] = t[0] ^ self.state[90] & self.state[91] ^ self.state[170]
+            t[1] = t[1] ^ self.state[174] & self.state[175] ^ self.state[263]
+            t[2] = t[2] ^ self.state[285] & self.state[286] ^ self.state[68]
+            
+            self.state[0:92] = [t[2], *self.state[0:91]]
+            self.state[93:176] = [t[0], *self.state[93:175]]
+            self.state[177:287] = [t[1], *self.state[177:286]]
+            # print(len(z))
+            #print(''.join([str(x) for x in self.state]))
+        # plaintext = bin(bytes_to_long(plaintext.encode()))[2:].zfill(N)
+        plaintext = bin(int(plaintext[2:],16))[2:].zfill(N)
+        cipher = [a^int(b) for a,b in zip(z, plaintext)]
+        cipher = ''.join([str(x) for x in cipher]).encode()
+        cipher = hex(int(cipher,2))
+        return cipher, self.key, self.iv 
+
+    def encrypt(self, plaintext):
+        self.generate_key()
+        return self.algorithm(plaintext)
+
+    def decrypt(self, ciphertext):
+        return self.algorithm(ciphertext)
+        
+if __name__ == "__main__":
+    triv = Trivium()
+    plaintext = "Hello"
+    plaintext = hex(int(plaintext.encode().hex(),16))
+    print(plaintext)
+    n, key, iv = triv.encrypt(plaintext)
+    print(n)
+    print('key:',''.join([str(x) for x in key]))
+    print('iv:',''.join([str(x) for x in iv]))
+    print(n)
+    n, key, iv = triv.decrypt(n)
+    print(n)
